832db868a392ddbf8877bd88b6f8c32d5af2545e
SUPERTKY <purple_chips@outlook.jp>
2026-05-21 15:51:35 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -125,7 +125,7 @@ const prizeHeightOffsetByTypeId = {
   7: -0.15,
   8: -0.1,
   9: -0.1,
-  10: -0.1,
+  10: -0.15,
 };
 const prizeTypeConfigs = Array.from({ length: maxPrizeCount }, (_, index) => {
   const id = index + 1;
